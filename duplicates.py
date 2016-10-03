f78dc2055ca9c4f41895dbf5dc7516af6811179a
Ruslan Vorobyev <bodyrof1@gmail.com>
2016-10-03 17:47:32 +0300

    Updated code

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,29 @@
+import os
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def are_files_duplicates(file_path1):
+    dir_list = []
+    vocabular = {}
+    for tup in os.walk(file_path1):
+        dir_list.append(tup)
+        for lfile in tup[2]:
+            if lfile in vocabular:
+                index = 0
+                for file_size in vocabular[lfile]:
+                    if file_size == os.path.getsize(tup[0] + '/' + lfile):
+                        print('Файл', lfile, 'с размером', file_size,
+                              'байт существует. Расположение его дубликата: в папке', tup[0])
+                        break
+
+                    if index == len(vocabular[lfile]) - 1:
+                        vocabular[lfile].append(
+                            os.path.getsize(tup[0] + '/' + lfile))
+                        break
+                    index += 1
+            else:
+                vocabular[lfile] = [os.path.getsize(tup[0] + '/' + lfile)]
 
 
 if __name__ == '__main__':
-    pass
+    pdir = input()
+    are_files_duplicates(pdir)
